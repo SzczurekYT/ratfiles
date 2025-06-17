370cf7ec1ff569f7e49ec18955fe0ec95648e575
SzczurekYT <szczurek@szczurek.yt>
2025-06-17 19:31:07 +0200

    waybar: a bit of styling

--- nix/home.nix
+++ nix/home.nix
@@ -24,6 +24,22 @@ in
 
   programs.waybar = {
     enable = true;
+    style = ''
+      ${builtins.readFile "${pkgs.waybar}/etc/xdg/waybar/style.css"}
+
+      window#waybar {
+          background-color: rgba(30, 30, 46, 0.75); /* Catppuccin Mocha 75% alpha */
+          /* border-bottom: 2px solid rgba(89, 89, 89, 0.667); Hyprland default border color */
+          border-bottom: 2px solid rgb(153, 224, 255); /* My color */
+      }
+
+      #clock {
+        color: rgb(153, 224, 255);
+        background-color: transparent;
+        font-size: 16px;
+        font-weight: bold;
+      }
+    '';
     settings = [
       {
         height = 30;
