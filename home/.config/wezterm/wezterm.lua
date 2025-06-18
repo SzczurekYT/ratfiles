795e03c3ed635164229245611bcfe77e4dd2e4b4
SzczurekYT <szczurek@szczurek.yt>
2025-06-18 12:33:29 +0200

    wezterm: Add config

---
+++ home/.config/wezterm/wezterm.lua
@@ -0,0 +1,10 @@
+local config = {}
+config.window_background_opacity = 0.75
+config.default_cursor_style = 'SteadyBar'
+config.colors = { 
+	cursor_border = '#99e0ff',
+	cursor_bg = '#99e0ff'
+}
+config.enable_tab_bar = false
+config.color_scheme = "Catppuccin Mocha"
+return config
