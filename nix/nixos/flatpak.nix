33d93108e734807d9649025f3d888accf03fa463
SzczurekYT <szczurek@szczurek.yt>
2026-03-20 19:12:19 +0100

    feat: nixos: Virutal keyboard from kde

--- nix/nixos/flatpak.nix
+++ nix/nixos/flatpak.nix
@@ -1,10 +1,14 @@
 { lib, ... }: {
 
+  services.flatpak.remotes = [{
+    name = "plasma-keyboard-nightly"; location = "https://cdn.kde.org/flatpak/plasma-keyboard-nightly/plasma-keyboard-nightly.flatpakrepo";
+  }];
+
   services.flatpak.update.auto.enable = false;
   services.flatpak.uninstallUnmanaged = false;
 
   services.flatpak.packages = [
     "com.discordapp.Discord"
-  ];
-
+    "org.kde.plasma.keyboard"
+  ]; 
 }
