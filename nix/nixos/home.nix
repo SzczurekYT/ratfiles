12464402183f88bf37683777ed2d938c070f3310
SzczurekYT <szczurek@szczurek.yt>
2026-03-21 12:35:05 +0100

    feat: nixos: Git config

--- nix/nixos/home.nix
+++ nix/nixos/home.nix
@@ -8,6 +8,16 @@
   home.packages = [ pkgs.zsh ];
   programs.zsh.enable = true;
 
+  programs.git = {
+    enable = true;
+    settings = {
+      user.name = "SzczurekYT";
+      user.email = "szczurek@szczurek.yt";
+      init.defaultBranch = "master";
+      push.autoSetupRemote = true;
+    };
+  };
+
   # The state version is required and should stay at the version you
   # originally installed.
   home.stateVersion = "25.11";
