2e0fe0cdfe1e2c2ccb57065de49c35693e7d2abf
SzczurekYT <szczurek@szczurek.yt>
2026-04-06 11:10:49 +0200

    feat: nixos: Install and use Helix editor

--- nix/nixos/shell_config.nix
+++ nix/nixos/shell_config.nix
@@ -3,9 +3,22 @@
 {
   programs.nushell = {
     enable = true;
+    shellAliases = {
+      vi = "hx";
+      vim = "hx";
+      nvim = "hx";
+      nano = "hx";
+    };
   };
 
   programs.starship = {
     enable = true;
+  };
+
+  programs.helix = {
+    enable = true;
+    settings = {
+      theme = "catppuccin_mocha";
+    };
   };
 }
