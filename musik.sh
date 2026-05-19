38ee992992abdb54c975b30d5f2e16f34515d43e
SzczurekYT <szczurek@szczurek.yt>
2026-05-19 10:09:39 +0200

    feat: Improve musik script to not launch if no musik file

--- musik.sh
+++ musik.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
+
+MUSIK_FILE="./musik_file"
+
+cd "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+
+if [ ! -f "$MUSIK_FILE" ]; then
+	exit
+fi
+
 if kdialog --title "Musik?" --yesno "Play musik?" ; then
-	mpv --loop --volume=35 --force-window --wayland-app-id=mpv-musik /home/szczurek/github/ratfiles/musik_file &
+	mpv --loop --volume=35 --force-window --wayland-app-id=mpv-musik "$MUSIK_FILE" &
 	sleep 1
 	hyprctl dispatch movetoworkspacesilent 10,class:mpv-musik
 fi
