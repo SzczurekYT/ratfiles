7e5a0f82a29ec19c0cc5c233280a1be16e4601d6
SzczurekYT <szczurek@szczurek.yt>
2026-06-21 18:59:00 +0200

    feat(nix): Add swap to rat-laptok

--- nix/systems/rat-laptok/system.nix
+++ nix/systems/rat-laptok/system.nix
@@ -17,6 +17,11 @@
     })
   ];
 
+  swapDevices = [{
+    device = "/var/lib/swapfile";
+    size = 16*1024;
+  }];
+
   hardware.sensor.iio.enable = true;
   hardware.bluetooth.enable = true;
   hardware.amd-npu.enable = true;
